1bdc0c916a4b1953e6b2b7246fdf58bdf1c85c68
Melina <mrequena@fue.undef.edu.ar>
2023-05-10 17:42:40 -0300

    Amigos!!!

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 #include"Persona.h"
 
-
 int main(){
-cargarPersonas();
-}
+    Persona * Melina = newPersona(45924375);
+    Persona *  Barbie = newPersona(22656451);
+    Persona * Sergio = newPersona(21697847);
+    Persona * LilaNTLP = newPersona(49551859);
 
+    Melina ->amigo = LilaNTLP;
+    LilaNTLP ->amigo = Melina;
+    Barbie ->amigo = LilaNTLP;
+    Barbie ->amigo = Melina;
+    Sergio ->amigo = LilaNTLP;
+    Sergio ->amigo = LilaNTLP;
+    return 0;
+}
